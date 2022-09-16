93e6737354e6e1907efa888338b8179cf036d99a
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 11:07:35 +0300

    add HomeWork about dog

---
+++ HomeWork_DogRun/Program.cs
@@ -0,0 +1,23 @@
+﻿int count=0, friend=2;
+double distance=10000,
+        firstFriendSpeed=1, secondFriendSpeed=2,
+        dogSpeed=5,
+        time=0;
+while (distance>10)
+{
+    if (friend==1)
+    {
+        time=distance/(secondFriendSpeed+dogSpeed);
+        friend=2;
+    }
+    else
+    {
+        time=distance/(firstFriendSpeed+dogSpeed);
+        friend=1;
+    }
+    distance+=-(firstFriendSpeed+secondFriendSpeed)*time;
+    count++;
+}
+
+Console.Write ("Собака пробежит ");
+Console.WriteLine (count);
