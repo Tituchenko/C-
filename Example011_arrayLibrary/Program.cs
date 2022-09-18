fefbf0af0414b0c90e5888a38bebd0293cc152f1
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-18 22:34:07 +0300

    add Ex 011 arrat library array random generator and print this

---
+++ Example011_arrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿void fillArray (int[] collection)
+{
+    int length=collection.Length;
+    int index=0;
+    while (index<length)
+    {
+        collection[index]=new Random().Next(1,10);
+        index++;
+    }
+}
+
+void printArray (int[] col)
+{
+    int cont=col.Length;
+    int ind=0;
+    Console.Write ("(");
+    while (ind<cont)
+    {
+        Console.Write ($"[{ind}]->{col[ind]}");
+        ind++;
+        if (ind!=cont) Console.Write (",");
+    }
+    Console.WriteLine (")");
+}
+
+int[] array = new int [10];
+fillArray(array);
+printArray (array);
