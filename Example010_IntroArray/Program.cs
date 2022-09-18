e77af57a029b60989bc439fe1473ab89f950edaf
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-18 22:11:13 +0300

    Add Ex 010 Intro Array find index

---
+++ Example010_IntroArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array={16,62,35,4,51,62,57,48,95};
+int n=array.Length;
+
+int find=4;
+
+int index=0;
+
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
