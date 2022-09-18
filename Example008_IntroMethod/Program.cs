b9cbb24e527580d158d1be8f55252956b7a9c985
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-18 21:41:37 +0300

    Ex 008 used func Max

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,4 +1,11 @@
-﻿int a1=11;
+﻿int Max (int arg1,int arg2,int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+int a1=11;
 int b1=67;
 int c1=87;
 int a2=21;
@@ -8,17 +15,10 @@ int a3=34;
 int b3=89;
 int c3=76;
 
-int max=a1;
+int max1=Max(a1,b1,c1);
+int max2=Max(a2,b2,c2);
+int max3=Max(a3,b3,c3);
 
-if (b1>max) max=b1;
-if (c1>max) max=c1;
+int finalMax=Max(max1,max2,max3);
 
-if (a2>max) max=a2;
-if (b2>max) max=b2;
-if (c2>max) max=c2;
-
-if (a3>max) max=a3;
-if (b3>max) max=b3;
-if (c3>max) max=c3;
-
-Console.WriteLine ($"Максимальное число {max}");
+Console.WriteLine ($"Максимальное число {finalMax}");
