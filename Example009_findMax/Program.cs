f7321ed5e31ca19b01b694a7e6e577bb3373c6b2
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-18 22:01:36 +0300

    add Ex009 findMax from array of 9 numbers

---
+++ Example009_findMax/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max (int arg1,int arg2,int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+int[] array={15,52,38,488,56,68,77,877,97};
+
+int result=Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8]));
+
+Console.WriteLine ($"Максимальное число {result}");
