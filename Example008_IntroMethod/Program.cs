cc14b9e4543ac07cc3a374646a6b7220b6520cb8
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-18 21:47:19 +0300

    Ex008 used func programming

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -15,10 +15,10 @@ int a3=34;
 int b3=89;
 int c3=76;
 
-int max1=Max(a1,b1,c1);
-int max2=Max(a2,b2,c2);
-int max3=Max(a3,b3,c3);
+//int max1=Max(a1,b1,c1);
+//int max2=Max(a2,b2,c2);
+//int max3=Max(a3,b3,c3);
 
-int finalMax=Max(max1,max2,max3);
+int max=Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
 
-Console.WriteLine ($"Максимальное число {finalMax}");
+Console.WriteLine ($"Максимальное число {max}");
